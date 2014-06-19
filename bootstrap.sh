d869d11c18efe9fefd50ac1808ca382c0a48210d
Sam Smith <git@samsmith.io>
2014-06-19 15:20:48 +0100

    Initial commit
    * Add the ubuntu/trusty64 Vagrant box as a development environment
    * Install the latest and greatest libsodium-php

---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+sudo apt-get --yes update
+
+sudo apt-get --yes install php5 php5-dev
+
+# TODO (phuedx, 2014/06/19): Make the "Install libsodium" and "Install
+# libsodium-php" steps idempotent.
+
+# Install libsodium
+sudo apt-get --yes install git-core
+
+pushd /tmp
+
+git clone https://github.com/jedisct1/libsodium.git
+cd libsodium
+./autogen.sh
+./configure
+make check
+sudo make install
+cd .. # cd libsodium
+
+# Install libsodium-php
+git clone https://github.com/jedisct1/libsodium-php.git
+cd libsodium-php
+phpize
+./configure --with-libsodium
+NO_INTERACTION=1 make test
+sudo make install
+sudo bash -c "echo \"extension=libsodium.so\" >> /etc/php5/mods-available/libsodium.ini"
+sudo php5enmod libsodium
+cd .. # cd libsodium-php
+
+popd
